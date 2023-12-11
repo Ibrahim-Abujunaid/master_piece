a05b915fe7a2782fb0496c2bb9eed8ea26385cff
Ibrahim-Abujunaid <ibrahimabujunaid@gmail.com>
2023-12-11 23:35:41 +0300

    inshallah

--- master peice/index.js
+++ master peice/index.js
@@ -5,7 +5,6 @@ menu.onclick = () => {
   nav1.classList.toggle('activ');
 }
 //       counter
-
 function find() {
     d=document.getElementById('driver').value;
     l=document.getElementById('location').value;
@@ -13,9 +12,9 @@ function find() {
     f=document.getElementById('fuel_type').value;
     p=document.getElementById('price').value;
     if (d==1) {
-        redirect=`CarWdriver/CarWdriver.html?price=${p}`;
+        redirect=`/CarWdriver/CarWdriver.html?price=${p}`;
     }else{
-        redirect=`shop/shop.html?price=${p}`;
+        redirect=`/shop/shop.html?price=${p}`;
     }
     redirect+= (l != 0) ? `&location=${l}` :'';
     redirect+= (b != 0) ? `&brand=${b}` :'';
@@ -23,6 +22,7 @@ function find() {
     console.log(redirect);
  location.href=redirect;
 }
+
 
  // Fetch data from the API
  fetch('http://127.0.0.1:8000/api/count')
@@ -48,7 +48,7 @@ function find() {
          </div>
          <div class="container">
              <img src="./img/icons/4.JPG" alt="">
-             <span class="num" data-value="30">0</span>
+             <span class="num" data-value="5">0</span>
              <span class="text"><i class="fa-solid fa-plus"></i>Years of Experience</span>
          </div>
      `;
@@ -63,7 +63,7 @@ valueDisplays.forEach((valueDisplay) => {
   let counter = setInterval(function () {
     startValue += 1;
     valueDisplay.textContent = startValue;
-    if (startValue >= endValue) {
+    if (startValue == endValue) {
       clearInterval(counter);
     }
   }, duration);
@@ -108,106 +108,159 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 fetch('http://127.0.0.1:8000/api/cars?withDriver=0')
-.then(response => response.json())
-.then(data => {
-    // Limit the number of cars to display
-    const carsToDisplay = data.slice(0, 4);
-    
-    // Populate the car container with fetched data
-    populateCarContainer(carsToDisplay);
-})
-.catch(error => console.error('Error fetching data:', error));
+    .then(response => response.json())
+    .then(data => {
+        // Limit the number of cars to display
+        const carsToDisplay = data.slice(0, 4);
+
+        // Populate the car container with fetched data
+        populateCarContainer(carsToDisplay);
+    })
+    .catch(error => console.error('Error fetching data:', error));
 
 function populateCarContainer(cars) {
-const carContainer = document.getElementById('carContainer');
+    const carContainer = document.getElementById('carContainer');
 
-// Iterate through the cars and generate HTML for each card
-cars.forEach(car => {
-    const carCard = document.createElement('div');
-    carCard.classList.add('arr-col');
+    // Iterate through the cars and generate HTML for each card
+    cars.forEach(car => {
+        // Fetch the average rating for the current car
+        fetch(`http://127.0.0.1:8000/api/Avg/${car.id}`)
+            .then(response => response.json())
+            .then(ratingData => {
+                const averageRating = ratingData.length > 0 ? parseFloat(ratingData[0].average_rating) : 0;
 
-    carCard.innerHTML = `
-        <div class="img">
-            <img src="http://127.0.0.1:8000/car/img/${car.img}" alt="${car.brand} Car">
-        </div>
-        <h5>${car.brand} Car</h5>
-        <div class="rating">
-            <div class="stars">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-            </div>
-            <div class="review">
-                <span>${car.name}</span>
-            </div>
-        </div>
-        <div class="features">
-            <span><i class="fa-solid fa-location-dot"></i>${car.location}</span>
-            <span><i class="fa-solid fa-gear"></i>${car.gear}</span>
-            <span><i class="fa-solid fa-bolt"></i>${car.fuel_type}</span>
-            <span><i class="fa-solid fa-car"></i>Car</span>
-        </div>
-        <div class="price">
-            <p>${car.price_day}JD/Day</p>
-            <a href='/SingleCar/SingleCar.html#${car.id}'><button>Rent Now</button></a>
-        </div>
-    `;
+                const carCard = document.createElement('div');
+                carCard.classList.add('arr-col');
 
-    carContainer.appendChild(carCard);
-});
+                carCard.innerHTML = `
+                    <div class="img">
+                        <img src="http://127.0.0.1:8000/car/img/${car.img}" alt="${car.brand} Car">
+                    </div>
+                    <h5>${car.brand} Car</h5>
+                    <div class="rating">
+                        <div class="stars">
+                            ${getStarIcons(averageRating)}
+                        </div>
+                        <div class="review">
+                            <span>${car.name}</span>
+                        </div>
+                    </div>
+                    <div class="features">
+                        <span><i class="fa-solid fa-location-dot"></i>${car.location}</span>
+                        <span><i class="fa-solid fa-gear"></i>${car.gear}</span>
+                        <span><i class="fa-solid fa-bolt"></i>${car.fuel_type}</span>
+                        <span><i class="fa-solid fa-car"></i>Car</span>
+                    </div>
+                    <div class="price">
+                        <p>${car.price_day}JD/Day</p>
+                        <a href='/SingleCar/SingleCar.html#${car.id}'><button>Rent Now</button></a>
+                    </div>
+                `;
+
+                carContainer.appendChild(carCard);
+            })
+            .catch(error => console.error('Error fetching average rating:', error));
+    });
 }
+
+// Helper function to generate star icons or display "New" based on the average rating
+function getStarIcons(averageRating) {
+    if (averageRating === 0) {
+        return 'New';
+    }
+  
+    const fullStars = Math.floor(averageRating);
+    const halfStar = averageRating - fullStars >= 0.5 ? 1 : 0;
+  
+    let starsHtml = '';
+  
+    for (let i = 0; i < fullStars; i++) {
+        starsHtml += '<i class="fa-solid fa-star"></i>';
+    }
+  
+    if (halfStar === 1) {
+        starsHtml += '<i class="fa-solid fa-star-half"></i>';
+    }
+  
+    return starsHtml !== '' ? starsHtml : 'New'; // Return 'New' if starsHtml is empty
+  }
+
 
 fetch('http://127.0.0.1:8000/api/cars?withDriver=1')
- .then(response => response.json())
- .then(data => {
-     // Limit the number of cars to display
-     const carsToDisplay = data.slice(0, 4);
-     
-     // Populate the car container with fetched data
-     populateCarContainer1(carsToDisplay);
- })
- .catch(error => console.error('Error fetching data:', error));
+    .then(response => response.json())
+    .then(data => {
+        // Limit the number of cars to display
+        const carsToDisplay = data.slice(0, 4);
+
+        // Populate the car container with fetched data
+        populateCarContainer1(carsToDisplay);
+    })
+    .catch(error => console.error('Error fetching data:', error));
 
 function populateCarContainer1(cars) {
- const carContainer = document.getElementById('carContainer1');
+    const carContainer = document.getElementById('carContainer1');
 
- // Iterate through the cars and generate HTML for each card
- cars.forEach(car => {
-     const carCard = document.createElement('div');
-     carCard.classList.add('arr-col');
+    // Iterate through the cars and generate HTML for each card
+    cars.forEach(car => {
+        // Fetch the average rating for the current car
+        fetch(`http://127.0.0.1:8000/api/Avg/${car.id}`)
+            .then(response => response.json())
+            .then(ratingData => {
+                const averageRating = ratingData.length > 0 ? parseFloat(ratingData[0].average_rating) : 0;
 
-     carCard.innerHTML = `
-         <div class="img">
-             <img src="http://127.0.0.1:8000/car/img/${car.img}" alt="${car.brand} Car">
-         </div>
-         <h5>${car.brand} Car</h5>
-         <div class="rating">
-             <div class="stars">
-                 <i class="fa-solid fa-star"></i>
-                 <i class="fa-solid fa-star"></i>
-                 <i class="fa-solid fa-star"></i>
-                 <i class="fa-solid fa-star"></i>
-                 <i class="fa-solid fa-star"></i>
-             </div>
-             <div class="review">
-                 <span>${car.name}</span>
-             </div>
-         </div>
-         <div class="features">
-             <span><i class="fa-solid fa-location-dot"></i>${car.location}</span>
-             <span><i class="fa-solid fa-gear"></i>${car.gear}</span>
-             <span><i class="fa-solid fa-bolt"></i>${car.fuel_type}</span>
-             <span><i class="fa-solid fa-car"></i>With driver</span>
-         </div>
-         <div class="price">
-             <p>${car.price_day}JD/Day</p>
-             <a href='/SingleCarWdriver/SingleCarWdriver.html#${car.id}'><button>Rent Now</button></a>
-         </div>
-     `;
-    
-     carContainer.appendChild(carCard);
- });
+                const carCard = document.createElement('div');
+                carCard.classList.add('arr-col');
+
+                carCard.innerHTML = `
+                    <div class="img">
+                        <img src="http://127.0.0.1:8000/car/img/${car.img}" alt="${car.brand} Car">
+                    </div>
+                    <h5>${car.brand} Car</h5>
+                    <div class="rating">
+                        <div class="stars">
+                            ${getStarIcons1(averageRating)}
+                        </div>
+                        <div class="review">
+                            <span>${car.name}</span>
+                        </div>
+                    </div>
+                    <div class="features">
+                        <span><i class="fa-solid fa-location-dot"></i>${car.location}</span>
+                        <span><i class="fa-solid fa-gear"></i>${car.gear}</span>
+                        <span><i class="fa-solid fa-bolt"></i>${car.fuel_type}</span>
+                        <span><i class="fa-solid fa-car"></i>Car</span>
+                    </div>
+                    <div class="price">
+                        <p>${car.price_day}JD/Day</p>
+                        <a href='/SingleCar/SingleCar.html#${car.id}'><button>Rent Now</button></a>
+                    </div>
+                `;
+
+                carContainer.appendChild(carCard);
+            })
+            .catch(error => console.error('Error fetching average rating:', error));
+    });
 }
+
+// Helper function to generate star icons or display "New" based on the average rating
+function getStarIcons1(averageRating) {
+    if (averageRating === 0) {
+        return 'New';
+    }
+  
+    const fullStars = Math.floor(averageRating);
+    const halfStar = averageRating - fullStars >= 0.5 ? 1 : 0;
+  
+    let starsHtml = '';
+  
+    for (let i = 0; i < fullStars; i++) {
+        starsHtml += '<i class="fa-solid fa-star"></i>';
+    }
+  
+    if (halfStar === 1) {
+        starsHtml += '<i class="fa-solid fa-star-half"></i>';
+    }
+  
+    return starsHtml !== '' ? starsHtml : 'New'; // Return 'New' if starsHtml is empty
+  }
 
--- master peice/profile/Orders/orders.js
+++ master peice/profile/Orders/orders.js
@@ -11,44 +11,92 @@ menu.onclick=()=>{
      name11.textContent=sessionStorage.getItem('name')
 
    // Fetch user ID from sessionStorage
-   const userId = sessionStorage.getItem('userId');
+  const userId = sessionStorage.getItem('userId');
 
-   // Fetch data from the API
-   fetch(`http://127.0.0.1:8000/api/rents/${userId}`)
-       .then(response => response.json())
-       .then(rentData => {
-           // Populate HTML structure for each rent
-           rentData.forEach(rent => {
-               // Extract only the date part
-               const startDate = rent.start.split(' ')[0];
-               const endDate = rent.end.split(' ')[0];
+fetch(`http://127.0.0.1:8000/api/rents/${userId}`)
+    .then(response => response.json())
+    .then(rentData => {
+        rentData.forEach(rent => {
+            const startDate = rent.start.split(' ')[0];
+            const endDate = rent.end.split(' ')[0];
 
-               var rentHtml = `
-                   <div class="card" data-rent-id="${rent.id}">
-                       <img src="http://127.0.0.1:8000/car/img/${rent.img}" class="card-img" alt="Car Image">
-                       <div class="card-body">
-                           <h3>${rent.brand}</h3>
-                           <p><strong>Landlord:</strong>${rent.landlord}</p>
-                           <p><strong>Rental Date:</strong>${startDate}</p>
-                           <p><strong>Return Date:</strong>${endDate}</p>
-                           <p><strong>Transmission:</strong>${rent.gear}</p>
-                           <p><strong>Fuel Type:</strong>${rent.fuel_type}</p>
-                           <p><strong>Location:</strong>${rent.location}</p>
-                           <p><strong>Driver:</strong>${rent.withDriver}</p>
-                           <p><strong>Cost:</strong>${rent.total_price} JD</p>
-                           <p class="status ${rent.status.toLowerCase()}">${rent.status}</p>
-                           <div class="actions">
-                               <i class="fa-solid fa-trash${ rent.status.toLowerCase()== 'accepted' ? 'invisible' :''}" onclick="deleteRent(${rent.id})"></i>
-                           </div>
-                       </div>
-                   </div>
-               `;
+            var rentHtml = `
+                <div class="card" data-rent-id="${rent.id}">
+                    <img src="http://127.0.0.1:8000/car/img/${rent.img}" class="card-img" alt="Car Image">
+                    <div class="card-body">
+                        <h3>${rent.brand}</h3>
+                        <p><strong>Landlord:</strong>${rent.landlord}</p>
+                        <p><strong>Rental Date:</strong>${startDate}</p>
+                        <p><strong>Return Date:</strong>${endDate}</p>
+                        <p><strong>Transmission:</strong>${rent.gear}</p>
+                        <p><strong>Fuel Type:</strong>${rent.fuel_type}</p>
+                        <p><strong>Location:</strong>${rent.location}</p>
+                        <p><strong>Driver:</strong>${rent.withDriver}</p>
+                        <p><strong>Cost:</strong>${rent.total_price} JD</p>
+                        <div class="stars" data-rent-id="${rent.id}">
+                            <form class="${rent.status.toLowerCase() == 'pending' ? 'invisible' : ''}">
+                                <input class="star star-5" id="star-5-${rent.id}" type="radio" name="rating" value="5" />
+                                <label class="star star-5" for="star-5-${rent.id}"></label>
+                                <input class="star star-4" id="star-4-${rent.id}" type="radio" name="rating" value="4" />
+                                <label class="star star-4" for="star-4-${rent.id}"></label>
+                                <input class="star star-3" id="star-3-${rent.id}" type="radio" name="rating" value="3" />
+                                <label class="star star-3" for="star-3-${rent.id}"></label>
+                                <input class="star star-2" id="star-2-${rent.id}" type="radio" name="rating" value="2" />
+                                <label class="star star-2" for="star-2-${rent.id}"></label>
+                                <input class="star star-1" id="star-1-${rent.id}" type="radio" name="rating" value="1" />
+                                <label class="star star-1" for="star-1-${rent.id}"></label>
+                            </form>
+                        </div>
+                        <p id="message-${rent.id}" class="message"></p>
+                        <p class="status ${rent.status.toLowerCase()}">${rent.status}</p>
+                        <div class="actions">
+                            <i class="fa-solid fa-trash${rent.status.toLowerCase() == 'accepted' ? 'invisible' : ''}" onclick="deleteRent(${rent.id})"></i>
+                        </div>
+                    </div>
+                </div>
+            `;
 
-               // Append the rentHtml to the rentsContainer
-               document.getElementById('rentsContainer').innerHTML += rentHtml;
-           });
-       })
-       .catch(error => console.error('Error fetching rents:', error));
+            document.getElementById('rentsContainer').innerHTML += rentHtml;
+        });
+    })
+    .catch(error => console.error('Error fetching rents:', error));
+
+// Attach a single event listener to the container for dynamic elements
+document.getElementById('rentsContainer').addEventListener('change', (event) => {
+    const target = event.target;
+
+    // Check if the changed element is a radio button
+    if (target.type === 'radio' && target.classList.contains('star')) {
+        const rentId = target.closest('.card').getAttribute('data-rent-id');
+        const rating = target.value;
+        submitRating(rentId, rating);
+    }
+});
+
+function submitRating(rentId, rating) {
+    const postData = {
+        rent_id: rentId,
+        rating: rating
+    };
+
+    fetch('http://127.0.0.1:8000/api/reviews', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json, text-plain, /',
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .then((data)  => {
+        document.getElementById(`message-${rentId}`).textContent="your rating has been added";
+
+        console.log('Rating submitted successfully:', data);
+    })
+    .catch(error => console.error('Error submitting rating:', error));
+}
+
 
        fetch(`http://127.0.0.1:8000/api/rejects/${userId}`)
        .then(response => response.json())
@@ -84,6 +132,9 @@ menu.onclick=()=>{
            });
        })
        .catch(error => console.error('Error fetching rents:', error));
+
+
+       
        function deleteRent1(rentId) {
         
         // Perform the deletion using the API
